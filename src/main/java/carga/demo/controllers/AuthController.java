24818735dbef4dc52bc6243917ba47fd136345f4
pmchavezc <pablomchavez620@gmail.com>
2025-05-10 16:16:26 -0600

    cambios

--- src/main/java/carga/demo/controllers/AuthController.java
+++ src/main/java/carga/demo/controllers/AuthController.java
@@ -4,7 +4,6 @@ import carga.demo.modelo.Cliente;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
--- src/main/java/carga/demo/controllers/ClienteController.java
+++ src/main/java/carga/demo/controllers/ClienteController.java
@@ -10,14 +10,8 @@ import carga.demo.servicios.UsuarioServiceImpl;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class ClienteController {
